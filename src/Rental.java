656a25d18dea6bb1fbf64b5fcfa3bc004cdf5836
Vibhav Gupta <vib7523@gmail.com>
2024-09-24 20:19:02 +0530

    added-LATE_PENALTY feature-2

--- src/Rental.java
+++ src/Rental.java
@@ -1,12 +1,17 @@
+import java.time.LocalDate;
 class Rental {
     private Car car;
     private Customer customer;
     private int days;
+    private LocalDate rentalDate;
+    private LocalDate dueDate;
 
     public Rental(Car car, Customer customer, int days) {
         this.car = car;
         this.customer = customer;
         this.days = days;
+        this.rentalDate = LocalDate.now();
+        this.dueDate = rentalDate.plusDays(days); // Due date is current date + rental days
     }
 
     public Car getCar() {
@@ -19,5 +24,13 @@ class Rental {
 
     public int getDays() {
         return days;
+    }
+
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
+
+    public LocalDate getRentalDate() {
+        return rentalDate;
     }
 }
